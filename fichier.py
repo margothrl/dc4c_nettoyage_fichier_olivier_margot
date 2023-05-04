3a7f5717472ea8873c7ed1f3262408e7447873fa
Margot Herail <margot.herail@hotmail.fr>
2023-05-04 17:10:29 +0200

    Étape 1 : suppr toutes les colonnes indésirables

--- fichier.py
+++ fichier.py
@@ -4,9 +4,10 @@ import pandas as pd
 # Voici donc comment nous avons fait :
 
 #Étape 1 : garder uniquement les colonnes : Period, Data_value et Series_title_2
-#df = df.drop(columns=['Series_reference', 'Suppressed', 'STATUS', 'UNITS', 'Magnitude', 'Subject', 'Group', 'Series_title_1', 'Series_title_3', 'Series_title_4', 'Series_title_5'],inplace=True)
 df = pd.read_csv("test.csv")
-df = df.drop(columns=["Series_reference"]) # supprimer la colone 1 pour tester
+#df = df.drop(columns=["Series_reference"]) # supprimer la colone 1 pour tester
+df = df.drop(columns=['Suppressed', 'STATUS', 'UNITS', 'Magnitude', 'Subject', 'Group', 'Series_title_1', 'Series_title_3', 'Series_title_4', 'Series_title_5']) #supprimer les autres colones
+
 df.to_csv("test.csv", index=False)
 
 #Étape 2 : filtrer les résultats en ne gardant que les résultats avec Credit, Debit et Services dans la colonne Series_title_2
